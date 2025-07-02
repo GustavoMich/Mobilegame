f8af60dac1bde5d495e69073433936e2061822c7
GustavoMich <gustavomichalski5@gmail.com>
2025-07-02 19:42:01 -0300

    obstacles
    Foi adicionado alguns obstáculos ao jogo.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+
     public float speed = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string tagToCheckEnemy = "Enemy";
 
-    // Update is called once per frame
+    private bool _canRun;
+    private Vector3 _pos;
+
+    
+    private void Start()
+    {
+        _canRun = true;
+    }
+    
     void Update()
     {
+        if (!_canRun) return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision colission)
+    {
+        if(colission.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        }
     }
 }
