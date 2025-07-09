0bb931b6ec6a6e1716c17ea85a6205467dc330e4
GustavoMich <gustavomichalski5@gmail.com>
2025-07-09 19:15:08 -0300

    animation speed change
    adicionado script para poder mudar a velocidade das animações.

--- Assets/Scripts/AnimationManager/AnimatorManager.cs
+++ Assets/Scripts/AnimationManager/AnimatorManager.cs
@@ -18,13 +18,14 @@ public class AnimatorManager : MonoBehaviour
    
     }
 
-    public void Play(AnimationType type)
+    public void Play(AnimationType type, float currentSpeedFactor = 1f)
     {
         foreach(var animation in animatorSetups)
         {
             if(animation.type == type)
             {
                 animator.SetTrigger(animation.trigger);
+                animator.speed = animation.speed * currentSpeedFactor;
                 break;
             }
         }
@@ -57,6 +58,7 @@ public class AnimatorSetup
 {
     public AnimatorManager.AnimationType type;
     public string trigger;
+    public float speed = .1f;
 }
 
 
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -31,6 +31,7 @@ public class PlayerController : Singleton<PlayerController>
     private Vector3 _pos;
     private float _currentSpeed;
     private Vector3 _startPosition;
+    private float _baseSpeedToAnimation = 4;
 
     public bool invencible = false;
 
@@ -91,7 +92,7 @@ public class PlayerController : Singleton<PlayerController>
     public void StartToRun()
     {
         _canRun = true;
-        animatorManager.Play(AnimatorManager.AnimationType.RUN);
+        animatorManager.Play(AnimatorManager.AnimationType.RUN, _currentSpeed / _baseSpeedToAnimation);
     }
 
     #region POWER UPS
