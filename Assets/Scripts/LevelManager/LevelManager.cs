09856ce6fa121cb0128e4c7752527946e75fd58e
GustavoMich <gustavomichalski5@gmail.com>
2025-07-16 16:34:58 -0300

    Level animation
    foi adicionado uma animação ao gerar o level.

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class LevelManager : MonoBehaviour
 
     public List<LevelPieceBasedSetup> levelPieceBasedSetups;
 
+    [Header("Animation")]
+    public float scaleDuration = .2f;
+    public float scaleTimeBetweenPieces = .1f;
+    public Ease ease = Ease.OutBack;
 
 
     
@@ -18,7 +23,7 @@ public class LevelManager : MonoBehaviour
     [SerializeField]private int _index;
     private GameObject _currentLevel;
 
-    private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
+    [SerializeField]private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     private LevelPieceBasedSetup _currSetup;
 
 
@@ -27,6 +32,7 @@ public class LevelManager : MonoBehaviour
     {
         //SpawnNextLevel();
         
+
 
     }
 
@@ -91,8 +97,27 @@ public class LevelManager : MonoBehaviour
         }
 
        ColorManager.Instance.ChangeColorByType(_currSetup.artType);
-        
 
+        StartCoroutine(ScalePiecesByTime());
+
+
+
+    }
+
+    IEnumerator ScalePiecesByTime()
+    {
+        foreach(var p in _spawnedPieces)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        yield return null;
+
+        for(int i = 0; i < _spawnedPieces.Count; i++)
+        {
+            _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
+        }
     }
 
     private void CreateLevelPiece(List<LevelPieceBase> list)
